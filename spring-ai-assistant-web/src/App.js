e112dcb0c92f557c7618e4d0a9769ecd0696bca8
SIddharth3791 <spatel3791@gmail.com>
2025-01-29 16:10:59 -0500

    Updated Home Screen to Support Multiple action button for AI Assistant

--- spring-ai-assistant-web/src/App.js
+++ spring-ai-assistant-web/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import react, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const[activeTab, setActiveTab] = useState('image-gen')
+
+  const handleTabChange = (tab) => {
+    console.log(tab);
+    setActiveTab(tab)
+  }
+
+  const checkIfctive = (selectedTab) => {
+    return activeTab ===selectedTab ? "active" : "inActive";
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className={checkIfctive("image-gen")} 
+              onClick={() => handleTabChange("image-gen")}>Image Generator
+      </button>
+      <button className={checkIfctive("chat")} 
+              onClick={() => handleTabChange("chat")}>Chat
+      </button>
+      <button className={checkIfctive("recipe-gen")} 
+              onClick={() => handleTabChange("recipe-gen")}>Recipe Generator
+      </button>
+
+      <div>
+        {activeTab === "image-gen" && <h2>CREATE AN IMAGE FOR ME</h2>}
+        {activeTab === "chat" && <h2>ASK ME ANYTHING</h2>}
+        {activeTab === "recipe-gen" && <h2>SUGGEST ME FOOD RECIPE</h2>}
+      </div>
     </div>
   );
 }
